bb496b68907eab4d324228b93310aea144151550
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-08-19 14:55:46 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
+// Replace YOUR-USERNAME and REPO-NAME
 export default defineConfig({
   plugins: [react()],
+  base: "/Geometry_mukil/",
 })
